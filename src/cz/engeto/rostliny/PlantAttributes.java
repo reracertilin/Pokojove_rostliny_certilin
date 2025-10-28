7c8f7378a2a0e8d2561d2525631274d6e3055540
rera0 <rera012@seznam.cz>
2025-10-28 20:52:30 +0100

    Initial commit

---
+++ src/cz/engeto/rostliny/PlantAttributes.java
@@ -0,0 +1,78 @@
+package cz.engeto.atributyRostlin;
+import java.time.LocalDate;
+
+public class AtributyRostlin {
+
+    private String name;
+    private String notes;
+    private LocalDate planted;
+    private LocalDate lastWatering;
+    private int frequencyOfWatering; //ve dnech
+
+    //region constructor
+    public AtributyRostlin(String name, String notes, LocalDate planted, LocalDate lastWatering, int frequencyOfWatering) {
+        this.name = name;
+        this.notes = notes;
+        this.planted = planted;
+        this.lastWatering = lastWatering;
+        this.frequencyOfWatering = frequencyOfWatering;
+    }
+    public AtributyRostlin(String name, int frequencyOfWatering) {
+        this (name, "Bez poznámky", LocalDate.now(), LocalDate.now(), frequencyOfWatering);
+    }
+    public AtributyRostlin(String name) {
+        this (name, "Bez poznámky", LocalDate.now(), LocalDate.now(), 7);
+    }
+    //endregion
+
+    //region Gettery a settery
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public LocalDate getPlanted() {
+        return planted;
+    }
+
+    public void setPlanted(LocalDate planted) {
+        this.planted = planted;
+    }
+
+    public int getFrequencyOfWatering() {
+        return frequencyOfWatering;
+    }
+
+    public void setFrequencyOfWatering(int frequencyOfWatering) {
+        this.frequencyOfWatering = frequencyOfWatering;
+    }
+
+    public LocalDate getLastWatering() {
+        return lastWatering;
+    }
+
+    public void setLastWatering(LocalDate lastWatering) {
+        this.lastWatering = lastWatering;
+    }
+    //endregion
+
+    public String getWateringInfo() {
+        return
+                "jméno rostliny: " + name + "\n" +
+                "poznámka: " + notes + "\n" +
+                "zasazená: " + planted + "\n" +
+                "naposled zalitá: " + lastWatering + "\n" +
+                "frekvence zalévání: " + frequencyOfWatering ;
+    }
+}
